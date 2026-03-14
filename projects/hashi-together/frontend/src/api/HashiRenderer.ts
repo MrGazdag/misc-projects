0e043165e9b060813c4718e25973e8c69bbd23f1
MrGazdag <44264503+MrGazdag@users.noreply.github.com>
2026-03-14 11:53:23 +0100

    hashi-together: better grid, coords

--- projects/hashi-together/frontend/src/api/HashiRenderer.ts
+++ projects/hashi-together/frontend/src/api/HashiRenderer.ts
@@ -6,9 +6,27 @@ import HashiCellState from "../../../common/HashiCellState";
 
 export default class HashiRenderer {
     private map: HashiMap;
+    private settings: HashiRendererSettings;
 
     constructor(map: HashiMap) {
         this.map = map;
+        this.settings = {
+            showCoords: true
+        };
+    }
+    private calculateCoordsText(chars: string, number: number) {
+        let len = chars.length;
+        let result = "";
+        if (number == 0) return chars[0];
+        while (true) {
+            let mod = number % len;
+            result = chars[mod] + result;
+            if (number < len) break;
+            number = (number - mod) / len;
+            // don't start from 1 when dividing, avoids "a, b, c, ba"
+            number -= 1;
+        }
+        return result;
     }
 
     render(r: RenderContext<CanvasRenderingContext2D>) {
@@ -22,23 +40,57 @@ export default class HashiRenderer {
 
         let mapSize = this.map.getMapSize();
 
-        let cW = w / (mapSize+1);
-        let cH = h / (mapSize+1);
+        let cW = w / (mapSize + 1 + (this.settings.showCoords ? 2 : 0));
+        let cH = h / (mapSize + 1 + (this.settings.showCoords ? 2 : 0));
 
         const marginX = cW / 2;
         const marginY = cH / 2;
 
+        // Grid
+        ctx.beginPath();
+        for (let i = 0; i < mapSize; i++) {
+            // Horizontal
+            ctx.moveTo(marginX+cW,   marginY+cH + (0.5+i)*cH);
+            ctx.lineTo(w-marginX-cW, marginY+cH + (0.5+i)*cH);
+
+            // Vertical
+            ctx.moveTo(marginX+cW + (0.5+i)*cW, marginY+cH);
+            ctx.lineTo(marginX+cW + (0.5+i)*cW, h-marginY-cH);
+        }
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#0003';
+        ctx.stroke();
+
+
+        ctx.fillStyle = "#0003";
+        if (this.settings.showCoords) {
+            ctx.font = cW*0.6 + "px Arial";
+            // Coordinates
+            for (let x = 0; x < mapSize; x++) {
+                let str = this.calculateCoordsText("abcdefghijklmnopqrstuvwxyz", x);
+                // Top
+                Utils.centerFillText(ctx, marginX+(1.5 + x)*cW, marginY + 0.5*cH, str);
+                // Bottom
+                Utils.centerFillText(ctx, marginX+(1.5 + x)*cW, h-marginY-0.5*cH, str);
+            }
+            for (let y = 0; y < mapSize; y++) {
+                let str = y+"";
+                // Left
+                Utils.centerFillText(ctx, marginX + 0.5*cH, marginY+(1.5 + y)*cH, str);
+                // Right
+                Utils.centerFillText(ctx, w-marginX-0.5*cH, marginY+(1.5 + y)*cH, str);
+            }
+        }
+
+        // Cells
         for (let i = 0; i < this.map.getCells().length; i++){
             let [x,y] = HashiUtils.indexToCoords(i, mapSize);
-            let centerX = marginX + (x+0.5) * cW;
-            let centerY = marginY + (y+0.5) * cW;
+            let centerX = marginX + (x+0.5+(this.settings.showCoords ? 1 : 0)) * cW;
+            let centerY = marginY + (y+0.5+(this.settings.showCoords ? 1 : 0)) * cH;
             let state = this.map.getCells()[i];
 
-            ctx.fillStyle = "#0003";
             const gridWidth = cW * 0.1;
             const gridHeight = cH * 0.1;
-            ctx.fillRect(centerX - cW/2, centerY - gridHeight/2, cW, gridHeight);
-            ctx.fillRect(centerX - gridWidth/2, centerY - cH/2, gridWidth, cH);
 
             if (HashiUtils.isCell(state)) {
                 let wasCrossedOut = HashiUtils.isCrossedCell(state);
@@ -99,4 +151,7 @@ export default class HashiRenderer {
 
         this.map.click(x, y, right);
     }
+}
+export interface HashiRendererSettings {
+    showCoords: boolean
 }
